af197fc58b88915eba02ef000ffb0df62682b1be
Sander Aernouts <sander.aernouts@gmail.com>
2018-08-22 21:34:18 +0200

    added mapping and more detailed query

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
 import * as tc from "telemetryclient-team-services-extension";
 import telemetryClientSettings = require("./telemetryClientSettings");
 import BuildApi = require("TFS/Build/RestClient");
+import Contracts = require("TFS/Build/Contracts");
 import Controls = require("VSS/Controls");
 import Grids = require("VSS/Controls/Grids");
+
+export class Row {
+    QueueTime: Date;
+    Definition: string;
+    BuildNumber: string;
+    Result: string;
+}
+
+function Map(build: Contracts.Build) {
+    let row = new Row();
+    row.QueueTime = build.queueTime;
+    row.Definition = build.definition.name;
+    row.BuildNumber = build.buildNumber;
+
+    switch (build.result) {
+        case Contracts.BuildResult.Succeeded:
+        row.Result = "succeeded";
+        break;
+
+        case Contracts.BuildResult.PartiallySucceeded:
+        row.Result = "partially succeeded";
+        break;
+
+        case Contracts.BuildResult.Failed:
+        row.Result = "failed";
+        break;
+
+        case Contracts.BuildResult.Canceled:
+        row.Result = "canceled";
+        break;
+
+        case Contracts.BuildResult.None:
+        row.Result = "none";
+        break;
+
+        default:
+        row.Result = "unkown";
+        break;
+    }
+
+    return row;
+}
 
 function Render(WidgetHelpers, widgetSettings) {
     const projectId = VSS.getWebContext().project.id;
@@ -10,50 +53,49 @@ function Render(WidgetHelpers, widgetSettings) {
     const container = $("#query-info-container");
     container.empty();
 
+    BuildApi.getClient().getBuilds(
+        projectId,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        Contracts.BuildStatus.Completed,
+        Contracts.BuildResult.Failed | Contracts.BuildResult.PartiallySucceeded,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        1,
+        Contracts.QueryDeletedOption.ExcludeDeleted ,
+        Contracts.BuildQueryOrder.FinishTimeDescending
+    ).then(builds  => {
+    const container = $("#query-info-container");
+    container.empty();
+
+    let rows = builds.map(x => Map(x));
+
     let gridOptions: Grids.IGridOptions = {
         height: "100%",
         width: "100%",
-        source: function () {
-          let result = [], i;
-          for (i = 0; i < 100; i++) {
-            result[result.length] = [i, "Column 2 text" + i, "Column 3 " + Math.random()];
-          }
+        source: rows,
+        columns: [
+            { text: "Time", width: 200, index: "QueueTime" },
+            { text: "Definition", width: 200, index: "Definition"},
+            { text: "Build", width: 200, index: "BuildNumber" },
+            { text: "Result", width: 200, index: "Result" }
+        ]
+    };
 
-          return result;
-          } (),
-          columns: [
-            { text: "Column 1", index: 0, width: 50 },
-            { text: "Column 2", index: 1, width: 200, canSortBy: false },
-            { text: "Column 3", index: 2, width: 450 }]
-        };
-
-        let grid = Controls.create(Grids.Grid, container, gridOptions);
-        return WidgetHelpers.WidgetStatusHelper.Success();
-    /*
-        BuildApi.getClient().getBuilds(projectId).then(function(builds) {
-        const container = $("#query-info-container");
-        container.empty();
-
-        let gridOptions: Grids.IGridOptions = {
-            height: "100%",
-            width: "100%",
-            source: builds,
-            columns: [
-                { text: "Time", width: 200, index: "queueTime" },
-                { text: "Definition", width: 200, index: "definition"},
-                { text: "Build", width: 200, index: "buildNumber" },
-                { text: "Result", width: 200, index: "result" }
-            ]
-        };
-
-        Controls.create(Grids.Grid, container, gridOptions);
-        return WidgetHelpers.WidgetStatusHelper.Success();
+    Controls.create(Grids.Grid, container, gridOptions);
+    return WidgetHelpers.WidgetStatusHelper.Success();
 
     }, function (error) {
         // Use the widget helper and return failure as Widget Status
         return WidgetHelpers.WidgetStatusHelper.Failure(error.message);
     });
-    */
 }
 
 VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
